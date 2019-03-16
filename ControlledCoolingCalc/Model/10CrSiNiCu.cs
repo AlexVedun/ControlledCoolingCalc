c29dcc054567c9f69b884034adba2db312b83c2f
Efremenko Alexey <alexeyefr@gmail.com>
2019-03-16 21:58:39 +0200

    First release

---
+++ ControlledCoolingCalc/Model/10CrSiNiCu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlledCoolingCalc.Model
+{
+    public class _10CrSiNiCu : Steel
+    {
+        public _10CrSiNiCu()
+        {
+            ratio = 2.337;
+        }
+
+        public override void Calculate()
+        {
+            delta = tempBeginCooling - tempEndCooling;            
+            totalWaterFlow = -37.392 * tempWater + 30.26 * thickness + 39.79 * sectionCount - 1.7979 * delta + 29.861 * coolingRate;
+            waterFlowDown = totalWaterFlow / (ratio + 1);
+            waterFlowUp = waterFlowDown * ratio;
+            rollerSpeed = 0.09968 * sectionCount + (-0.01422) * thickness + (-0.00295) * delta + 0.00038 * waterFlowUp + 3.78E-5 * waterFlowDown + 0.00593 * tempWater + 0.05506 * coolingRate;
+        }
+    }
+}
---
+++ ControlledCoolingCalc/Model/K60.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlledCoolingCalc.Model
+{
+    public class K60 : Steel
+    {
+        public K60()
+        {
+            ratio = 2.4;
+        }
+
+        public override void Calculate()
+        {
+            delta = tempBeginCooling - tempEndCooling;
+            totalWaterFlow = 37.556 * tempWater + 36.217 * thickness - 60.56 * sectionCount + 0.0198 * delta + 23.109 * coolingRate;
+            waterFlowDown = totalWaterFlow / (ratio + 1);
+            waterFlowUp = waterFlowDown * ratio;
+            rollerSpeed = 0.1609 * sectionCount - 0.01448 * thickness - 0.00747 * delta - 0.00045 * waterFlowUp + 0.000719 * waterFlowDown - 0.01243 * tempWater + 0.06658 * coolingRate;
+        }
+    }
+}
